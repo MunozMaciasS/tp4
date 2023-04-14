d662fa71c6886361dd6900a1fe44180bc4149b94
MunozMaciasS <112488064+MunozMaciasS@users.noreply.github.com>
2023-04-14 15:27:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SCREEN_HEIGHT = 600
 COLORS = [arcade.color.BLUE, arcade.color.FANDANGO_PINK,
          arcade.color.FRENCH_ROSE, arcade.color.GOLDEN_POPPY, arcade.color.AERO_BLUE,arcade.color.BITTERSWEET_SHIMMER]
 
-class Cercle():
+class Cercle(): # on defnine un cercle par son rayon, son centre dans les axes de x et y, sa couleur, et son mouvement(dans es 2 axes)
    def __init__(self,rayon,center_x,center_y,color,change_x,change_y):
        self.rayon = rayon
        self.center_x = center_x
@@ -21,7 +21,7 @@ class Cercle():
        self.change_y = change_y
 
 
-   def draw(self):
+   def draw(self): #on dessine le cercle dans la fenetre
        arcade.draw_circle_filled(self.center_x,self.center_y,self.rayon,self.color)
        pass
 
@@ -38,7 +38,7 @@ class Cercle():
        if self.center_y - self.rayon < 0:
            self.change_y *= -1
 
-class Rectangle():
+class Rectangle():#on definie un rectangle par sa base et sa hauteur,son angle par rapport le cadre son centre dans les axes de x et y, sa couleur, et son mouvement(dans es 2 axes)
     def __init__(self,base,heigth,center_x,center_y,color,change_x,change_y,angle):
         self.base = base
         self.heigth = heigth
@@ -49,7 +49,7 @@ class Rectangle():
         self.change_y = change_y
         self.angle = angle
 
-    def draw(self):
+    def draw(self): 
        arcade.draw_rectangle_filled(self.center_x,self.center_y,self.base,self.heigth,self.color,self.angle)
        pass
 
@@ -68,15 +68,15 @@ class Rectangle():
 
 
 
-class MyGame(arcade.Window):
+class MyGame(arcade.Window): #cette clase sert a definir la fenetre dont tout les action se deroules
    def __init__(self):
        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
-       self.liste_cercles = []
-       self.liste_rectangles = []
+       self.liste_cercles = [] #on cree une liste pour les cercles
+       self.liste_rectangles = [] #et ine autres pour les rectangles
 
 
    def setup(self):
-       # remplir la liste avec 20 objets de type Cercle
+       # remplir la liste avec 20 objets de type Cercle, avec leur respectives variables
        for _ in range(20): #on crée tout les variables des cercles et rectnagles pour apres les append à leur liste respectives
            rayon = random.randint(10,30)
            center_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
@@ -86,6 +86,7 @@ class MyGame(arcade.Window):
            change_y = random.randint(-10,10)
            self.liste_cercles.append(Cercle(rayon,center_x,center_y,color, change_x, change_y)) #on appelle le contrustor et on append l'object crée
        for _ in range(20):
+         #  remplir la liste avec 20 objets de type Cercle, avec leur respectives variables
            base = random.randint(10, 30)
            heigth = random.randint(10, 30)
            center_x = random.randint(0 + base, SCREEN_WIDTH - base)
@@ -112,8 +113,8 @@ class MyGame(arcade.Window):
            rectangle.mouvement()
 
    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-
-       if button == arcade.MOUSE_BUTTON_LEFT: #lorseque on clique la partie gauche du mouse, on append un cerlce
+#
+       if button == arcade.MOUSE_BUTTON_LEFT: #lorseque on clique la partie gauche du mouse,on append un cercle(avec ses proprietes) dans la position du click
            rayon = random.randint(10, 30)
            center_x = x
            center_y =y
@@ -121,7 +122,7 @@ class MyGame(arcade.Window):
            change_x = random.randint(-10, 10)
            change_y = random.randint(-10, 10)
            self.liste_cercles.append(Cercle(rayon, center_x, center_y, color, change_x, change_y))
-       elif button == arcade.MOUSE_BUTTON_RIGHT: #lorseque on clique la partie droit du mouse, on append un rectangle
+       elif button == arcade.MOUSE_BUTTON_RIGHT: #lorseque on clique la partie droit du mouse,on append un rectangle(avec ses proprietes) dans la position du click
            base = random.randint(10, 30)
            heigth = random.randint(10, 30)
            center_x = x
@@ -139,7 +140,7 @@ class MyGame(arcade.Window):
 
 
 
-def main():
+def main(): #on run la fenetre
    my_game = MyGame()
    my_game.setup()
 
